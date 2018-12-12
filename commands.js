987e860ba952e89e53354a33b608c8a13b1a0e7e
Marcus Little <mcolelittle@gmail.com>
2018-12-12 08:21:22 -0700

    adding functionality for head command

--- commands.js
+++ commands.js
@@ -14,6 +14,10 @@ function evaluateCmd(userInput) {
         case "echo":
         commandLibrary.echo(userInputArray.slice(1).join(" "));
         break;
+        case "cat":
+        commandLibrary.cat(userInputArray.slice(1));
+        case "head":
+        commandLibrary.head(userInputArray.slice(1));
     }
 }
 
@@ -21,6 +25,22 @@ const commandLibrary = {
 
     "echo": function(userInput) {
         done(userInput);
+    },
+
+    "cat": function(fullPath) {
+        const fileName = fullPath[0];
+        fs.readFile(fileName, (err, data) => {
+            if(err) throw err;
+            done(data);
+        });
+    },
+
+    "head": function(fullPath) {
+        const headFile = fullPath[0].split('\n').slice(0, 2).join(" ");
+        fs.readFile(headFile, (err, data) => {
+            if(err) throw err;
+            done(data);
+        });
     }
 };
 
